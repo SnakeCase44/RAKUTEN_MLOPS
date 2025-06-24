931b4784f0601b0be6b12e3ef005b22a63efabdd
snakecase <snakecase44@gmail.com>
2025-06-24 17:24:13 +0200

    [#main] Changed : setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rakuten_ds",
+    name="rakuten_mlops",
     version="0.1",
-    packages=find_packages(include=["app", "app.*"]),
+    packages=find_packages(include=["models", "models.*"]),
 )
