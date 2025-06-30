b356b638dd8c33391e97e586c64122f99df23cf8
Valmdatascientest <milliet.valentin@orange.fr>
2025-06-30 10:54:57 +0200

    remise en place du yml de base

--- compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if command -v nvidia-smi &> /dev/null && nvidia-smi -L | grep -q "GPU"; then
-  echo "✅ GPU détecté. Lancement avec support GPU..."
-  docker-compose -f docker-compose.yml -f docker-compose.gpu.yml up
-else
-  echo "⚠️  Aucun GPU détecté. Lancement standard..."
-  docker-compose up
-fi
