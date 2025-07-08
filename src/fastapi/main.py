5a21c191314b7be0f789baa6b91d102220dc626e
L Catt <cattoire.jobs@gmail.com>
2025-07-08 23:37:12 +0200

    #14 added pages nginx (api ok mais pas les autres)

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -3,7 +3,10 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.fastapi.endpoints import test_api, prediction_api, api_train, api_evaluate
 
-app = FastAPI(title="Rakuten Multimodal API")
+app = FastAPI(
+    title="Rakuten Multimodal API",
+    root_path="/api"
+)
 
 # --- Prometheus Instrumentation ---
 Instrumentator().instrument(app).expose(app)
